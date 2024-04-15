8f7285891e2571281dbe8bfb4d95f77c79788ae3
FQS <95135822@qq.com>
2024-04-15 19:28:27 +0800

    auto import

---
+++ src/pages/home/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="back-btn-wrap"></div>
+</template>
+<script lang="ts" setup>
+interface propsType {
+  modelValue: string
+}
+
+const props = withDefaults(defineProps<propsType>(), {
+  modelValue: ''
+})
+
+const emit = defineEmits(['update:modelValue'])
+let InternalValue = computed({
+  get: () => {
+    return props.modelValue
+  },
+
+  set: (value: string) => {
+    emit('update:modelValue', value)
+  }
+})
+</script>
+<style lang="scss" scoped>
+.back-btn-wrap {
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from "vite";
-import uni from "@dcloudio/vite-plugin-uni";
+import { defineConfig } from 'vite'
+import uni from '@dcloudio/vite-plugin-uni'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()],
-});
+  plugins: [
+    AutoImport({
+      imports: ['vue', 'vue-router', 'pinia'],
+      resolvers: [],
+      dts: true,
+      eslintrc: {
+        enabled: true
+      }
+    }),
+    Components({
+      resolvers: []
+    }),
+    uni()
+  ]
+})
